5dd5ce6243f10278360904c0653a3c8990436a99
two0008 <42622541+two0008@users.noreply.github.com>
2020-02-09 06:38:10 -0600

    Update to the Game Manager: Encounters
    Finished the initialization of the encounters. - TWO

--- gameManager.java
+++ gameManager.java
@@ -38,16 +38,16 @@ import java.util.Arrays;
          int NUM_ENCOUNTERS = 5;
 
          Scanner input = new Scanner(System.in);
-         System.out.println("What would would like the first character's name to be?\n>> ")
+         System.out.println("What would would like the first character's name to be?\n>> ");
          String name1 = input.nextLine();
          character c1 = new character(String name1);
-         System.out.println("What would would like the second character's name to be?\n>> ")
+         System.out.println("What would would like the second character's name to be?\n>> ");
          String name2 = input.nextLine();
          character c2 = new character(String name2);
-         System.out.println("What would would like the third character's name to be?\n>> ")
+         System.out.println("What would would like the third character's name to be?\n>> ");
          String name3 = input.nextLine();
          character c3 = new character(String name3);
-         System.out.println("What would would like the fourth character's name to be?\n>> ")
+         System.out.println("What would would like the fourth character's name to be?\n>> ");
          String name4 = input.nextLine();
          character c4 = new character(String name4);
 
@@ -55,13 +55,60 @@ import java.util.Arrays;
          int charactersAlive = party.size();
 
          // INITIALIZATION OF encounters.
-         encounters a0 = new encounters();
-         encounters a1 = new encounters();
-         encounters a2 = new encounters();
-         encounters a3 = new encounters();
-         encounters a4 = new encounters();
+         encounters e0 = new encounters();
+         e0.setName("Meteor Shower");
+         e0.setDescription("An asteroid as broken up in the Martian atmosphere and a storm of meteriods "
+         + "is falling on the party's position. You have two options.");
+         e0.setOption1("Stop moving and take shelter");
+         int[] e01mod = {0,0,0,-10,50};
+         e0.setOption1Modifiers(e01mod);
+         e0.setOption2("Continue moving and brute force it");
+         int[] e02mod = {-50,0,0,0,150};
+         e0.setOption2Modifiers(e02mod);
+         
+         encounters e1 = new encounters();
+         e1.setName("Sandstorm");
+         e1.setDescription("A storm on the Martian surface has kicked up a considerable amount of dust."
+         + " Sand blocks your vision and slows down the party's progress. You have two options.");
+         e1.setOption1("Stop moving and take shelter from the storm");
+         int[] e11mod = {0,0,0,-15,0};
+         e1.setOption1Modifiers(e11mod);
+         e1.setOption2("Push through the storm hoping it passes quickly");
+         int[] e12mod = {0,-10,0,0,100};
+         e1.setOption2Modifiers(e12mod);
 
-         List<encounters> encounterList = new ArrayList<>(Arrays.asList(a0, a1, a2, a3, a4)) 
+         encounters e2 = new encounters();
+         e2.setName("Canyon Crossing");
+         e2.setDescription("A deep canyon on the Martian surface blocks the party's path. You have two options.");
+         e2.setOption1("Try to go around the canyon"); 
+         int[] e21mod = {0,0,0,-15,50};
+         e2.setOption1Modifiers(e21mod);
+         e2.setOption2("Attempt to traverse the canyon");
+         int[] e22mod = {0,-10,0,0,150};
+         e2.setOption2Modifiers(e22mod);
+
+         encounters e3 = new encounters();
+         e3.setName("Punctured Wheel");
+         e3.setDescription("A rock in on the trail has stuck your tire, and punctured a hole in it."
+         + " You have two options.");
+         e3.setOption1("Try to replace the tire completly"); 
+         int[] e31mod = {0,0,-15,0,100};
+         e3.setOption2("Attempt to repair the hole");
+         int[] e32mod = {0,0,-5,0,200};
+         e3.setOption2Modifiers(e32mod);
+
+         encounters e4 = new encounters();
+         e4.setName("Space Pirates");
+         e4.setDescription("A party of nomadic space pirates has ambushed the party and are asking for traveling taxes."
+         + " You have two options.");
+         e4.setOption1("Refuse to give up supplies to the cutthroats");
+         int[] e41mod = {-50, 0, 0, 0, 0};
+         e4.setOption1Modifiers(e41mod);
+         e4.setOption2("Give into their demands and pay the tax");
+         int[] e42mod = {0,0,-15,-15,200};
+         e4.setOption2Modifiers(e42mod);
+
+         List<encounters> encounterList = new ArrayList<>(Arrays.asList(a0, a1, a2, a3, a4));
          
          // CONDITIONS
          boolean WIN_CONDITION = miles >= 10000;
@@ -79,7 +126,7 @@ import java.util.Arrays;
             int[] encounterAnswer = encounterList.get(encounterIDX).main();
             int[] roundStats = new int[5];
             for (int i = 0; i < stats.length; i++) {
-                roundStats[i] = stats[i] - encounterAnswer[i];
+                roundStats[i] = stats[i] + encounterAnswer[i];
             }
             stats = roundStats;
 
